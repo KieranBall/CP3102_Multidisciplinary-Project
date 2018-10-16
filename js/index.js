a3102b33760989ecd39b2474e57aa985703aa44b
kieran <kieran.ball@my.jcu.edu.au>
2018-10-16 14:54:05 +1000

    updating the info in cards

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-function on_card_click() {
+function on_card_click(card_id) {
+console.log(card_id);
+    document.getElementById("modal_title").innerText = card_id;
+    document.getElementById("modal_body").innerText = card_id;
 
 }
 
