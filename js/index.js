18673b55783abcc17f440af2b4d7287487c27ec8
kieran <kieran.ball@my.jcu.edu.au>
2018-10-23 11:02:48 +1000

    built better modal for storing info

--- js/index.js
+++ js/index.js
@@ -15,39 +15,30 @@ var description9 = "9";
 
     switch(card_id) {
         case "1":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description1;
             break;
         case "2":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description2;
             break;
         case "3":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description3;
             break;
         case "4":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description4;
             break;
         case "5":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description5;
             break;
         case "6":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description6;
             break;
         case "7":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description7;
             break;
         case "8":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description8;
             break;
         case "9":
-            document.getElementById("modal_title").innerText = "";
             document.getElementById("modal_body").innerText = description9;
             break;
         default:
